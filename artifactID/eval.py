24d22fbeb89b7aaee768486e8fee4b6e2b81b357
Keerthi Sravan Ravi <ks3621@columbia.edu>
2020-06-23 15:14:27 +0530

    Add confusion matrix to eval
    And other minor refactoring

--- artifactID/eval.py
+++ artifactID/eval.py
@@ -6,17 +6,17 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 from tensorflow.keras.models import load_model
 
 from artifactID.common.data_ops import get_paths_labels, make_generator_inference
-
 
 # =========
 # TENSORFLOW INIT
 # =========
 # Prevent crash
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(gpus[0], True)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
 
 def _viz(paths, y_true, y_pred):
@@ -49,8 +49,10 @@ def main(batch_size: int, data_root: str, filter_artifact: str, model_load_path:
     # =========
     # Get paths and labels
     x_paths, y_labels = get_paths_labels(data_root=data_root, filter_artifact=filter_artifact)
-    dict_label_int = dict(zip(np.unique(y_labels), itertools.count(0)))
-    y_true = np.array([dict_label_int[label] for label in y_labels])
+    dict_label_int = dict(zip(np.unique(y_labels), itertools.count(0)))  # Map labels to int
+    dict_label_int = {'b0_': 0, 'noartifact': 1, 'snr': 2, 'wrap': 3}  # TODO REMOVE
+    # y_true = np.array([dict_label_int[label] for label in y_labels])  # TODO REMOVE
+    y_true = np.fromiter(map(lambda i: dict_label_int[i], y_labels), dtype=np.int8)  # Map labels to int
 
     # Split dataset
     test_pc = 0.10
@@ -78,6 +80,15 @@ def main(batch_size: int, data_root: str, filter_artifact: str, model_load_path:
     accuracy = len(np.where(y_true == y_pred)[0]) / len(y_true)  # Compute accuracy
     print(f'Accuracy: {accuracy}')
 
+    # =========
+    # CONFUSION MATRIX
+    # =========
+    dict_int_label = dict(zip(dict_label_int.values(), dict_label_int.keys()))
+    conf_matrix = confusion_matrix(y_true=y_true, y_pred=y_pred)
+
+    # =========
+    # SAVE EVAL RUN TO LOG
+    # =========
     # Append evaluation run to log
     log_path = list(Path(model_load_path).parts)
     log_path[-1] = 'log.txt'
@@ -86,14 +97,18 @@ def main(batch_size: int, data_root: str, filter_artifact: str, model_load_path:
         time_string = datetime.now().strftime('%y%m%d_%H%M')  # Time stamp when saving model
         write_str = f'\n{time_string} {accuracy * 100}% evaluation accuracy '
         file.write(write_str)
+        write_str += '\nConfusion matrix:\n'
+        write_str += str(conf_matrix)
+        write_str += '\n'
+        write_str += str(dict_int_label)
+        write_str += '========='
+        file.write(write_str)
 
     # =========
     # VISUALIZING EVALUATION RESULTS
     # =========
-    # Convert int to labels
-    dict_int_label = dict(zip(dict_label_int.values(), dict_label_int.keys()))
-    y_pred = list(map(lambda i: dict_int_label[i], y_pred))
-    _viz(paths=x_paths, y_true=y_labels, y_pred=y_pred)
+    y_pred_labels = np.fromiter(map(lambda i: dict_int_label[i], y_pred), dtype='U10')  # Map int to labels
+    _viz(paths=x_paths, y_true=y_labels, y_pred=y_pred_labels)
 
 
 if __name__ == '__main__':
