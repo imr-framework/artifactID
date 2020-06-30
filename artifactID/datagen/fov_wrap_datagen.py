0f7b061f9bab3fef393be34202713c382827ecb0
Keerthi Sravan Ravi <ks3621@columbia.edu>
2020-06-30 19:37:46 +0530

    Update FOV wrap-around datagen
    Ablate and discard central chunk in every volume since it should not be classified as wrap-around artifact

--- artifactID/datagen/fov_wrap_datagen.py
+++ artifactID/datagen/fov_wrap_datagen.py
@@ -9,6 +9,7 @@ from artifactID.common.data_ops import glob_brats_t1, glob_nifti, load_nifti_vol
 
 def main(path_read_data: str, path_save_data: str, patch_size: int):
     arr_wrap_range = [55, 60, 65, 70, 75, 80]
+    arr_wrap_range = [15, 20, 25, 30, 35]
 
     # =========
     # PATHS
@@ -20,8 +21,9 @@ def main(path_read_data: str, path_save_data: str, patch_size: int):
     path_save_data = Path(path_save_data)
     subjects_per_class = math.ceil(
         len(arr_path_read) / len(arr_wrap_range))  # Calculate number of subjects per class
-    arr_wrap_range = arr_wrap_range * subjects_per_class
-    np.random.shuffle(arr_wrap_range)
+    # arr_wrap_range = arr_wrap_range * subjects_per_class
+    arr_wrap_range = np.tile(arr_wrap_range, subjects_per_class)
+    # np.random.shuffle(arr_wrap_range)
 
     # =========
     # DATAGEN
@@ -30,23 +32,34 @@ def main(path_read_data: str, path_save_data: str, patch_size: int):
         vol = load_nifti_vol(path_t1)
         wrap = arr_wrap_range[ind]
         opacity = 0.5
-        top, middle, bottom = vol[:wrap], vol[wrap:-wrap], vol[-wrap:]
+
+        # Find extent of brain along axial
+        nonzero_idx = np.nonzero(vol)
+        first, last = nonzero_idx[0].min(), nonzero_idx[0].max()
+        vol_cropped = vol[first:last]
+
+        # Extract regions and construct overlap
+        top, middle, bottom = vol_cropped[:wrap], vol_cropped[wrap:-wrap], vol_cropped[-wrap:]
         middle[:wrap] += bottom * opacity
         middle[-wrap:] += top * opacity
-        middle = np.pad(middle, [[wrap, wrap], [0, 0], [0, 0]])
+
+        # Now extract the overlapping regions
+        # This is because the central unmodified region should not be classified as FOV wrap-around artifact
+        top, bottom = middle[:wrap], middle[-wrap:]
+        vol = np.append(top, bottom, axis=0)
 
         # Zero pad to compatible shape
         pad = []
-        shape = middle.shape
+        shape = vol.shape
         for s in shape:
             if s % patch_size != 0:
                 p = patch_size - (s % patch_size)
                 pad.append((math.floor(p / 2), math.ceil(p / 2)))
             else:
                 pad.append((0, 0))
-        middle = np.pad(array=middle, pad_width=pad)
+        vol = np.pad(array=vol, pad_width=pad)
 
-        patches = get_patches(arr=middle, patch_size=patch_size)  # Extract patches
+        patches = get_patches(arr=vol, patch_size=patch_size)  # Extract patches
 
         # Save to disk
         _path_save = path_save_data.joinpath(f'wrap{wrap}')
