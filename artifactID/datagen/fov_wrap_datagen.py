04cb5d0f5c7bc7be540ba4805ab83df8a648dc9b
Marina Manso Jimeno <marinaa246@gmail.com>
2020-10-01 17:46:27 -0400

    Implement wrap-around across all dimensions

--- artifactID/datagen/fov_wrap_datagen.py
+++ artifactID/datagen/fov_wrap_datagen.py
@@ -2,6 +2,7 @@ import math
 from pathlib import Path
 
 import numpy as np
+import random
 from tqdm import tqdm
 
 from artifactID.common import data_ops
@@ -29,26 +30,50 @@ def main(path_read_data: Path, path_save_data: Path, patch_size: int):
     # =========
     for ind, path_t1 in tqdm(enumerate(arr_path_read)):
         vol = data_ops.load_nifti_vol(path_t1)
+        # TODO: remove hardcoding
+        vol = np.rot90(np.moveaxis(vol, 0, -1), 2)
+        zero_pad = int((vol.shape[0] - vol.shape[1]) / 2)
+        vol = np.hstack(
+            (np.zeros((vol.shape[0], zero_pad, vol.shape[2])), vol, np.zeros((vol.shape[0], zero_pad, vol.shape[2]))))
+
         wrap = arr_wrap_range[ind]
         opacity = 0.5
+        direction = random.choice(['v', 'h', 'sl'])
 
         # Find extent of brain along axial
         nonzero_idx = np.nonzero(vol)
-        first, last = nonzero_idx[0].min(), nonzero_idx[0].max()
-        vol_cropped = vol[first:last]
 
         # Extract regions and construct overlap
-        top, middle, bottom = vol_cropped[:wrap], vol_cropped[wrap:-wrap], vol_cropped[-wrap:]
-        middle[:wrap] += bottom * opacity
-        middle[-wrap:] += top * opacity
+        if direction == 'v':
+            first, last = nonzero_idx[0].min(), nonzero_idx[0].max()
+            vol_cropped = vol[first:last]
+            top, middle, bottom = vol_cropped[:wrap], vol_cropped[wrap:-wrap], vol_cropped[-wrap:]
+            middle[:wrap] += bottom * opacity
+            middle[-wrap:] += top * opacity
 
-        # Now extract the overlapping regions
-        # This is because the central unmodified region should not be classified as FOV wrap-around artifact
-        top, bottom = middle[:wrap], middle[-wrap:]
+            # Now extract the overlapping regions
+            # This is because the central unmodified region should not be classified as FOV wrap-around artifact
+            wrap1, wrap2 = middle[:wrap], middle[-wrap:]
+        elif direction == 'h':
+            first, last = nonzero_idx[1].min(), nonzero_idx[1].max()
+            vol_cropped = vol[:, first:last]
+            left, middle, right = vol_cropped[:, :wrap], vol_cropped[:, wrap:-wrap], vol_cropped[:, -wrap:]
+            middle[:, -wrap:] += left * opacity
+            middle[:, :wrap] += right * opacity
+
+            wrap1, wrap2 = middle[:, :wrap], middle[:, -wrap:]
+
+        elif direction == 'sl':
+            first, last = nonzero_idx[1].min(), nonzero_idx[1].max()
+            vol_cropped = vol[:, :, first:last]
+            bottom_sl = vol_cropped[:, :, 1:wrap + 1]
+            opacity = 0.2 * np.linspace(1, 0.1, wrap)
+            wrap1 = vol_cropped[:, :, -wrap:] + np.flip(bottom_sl * opacity, axis=2)
+            wrap2 = np.zeros(wrap1.shape)
 
         # Now cycle through top and bottom chunks individually
         counter = 0
-        for vol in (top, bottom):
+        for vol in (wrap1, wrap2):
             # Zero-pad vol, get patches, discard empty patches and uniformly intense patches and normalize each patch
             vol = data_ops.patch_size_compatible_zeropad(vol=vol, patch_size=patch_size)
             patches, _ = data_ops.get_patches_per_slice(vol=vol, patch_size=patch_size)
