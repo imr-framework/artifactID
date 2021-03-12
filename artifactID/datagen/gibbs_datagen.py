808facc0ab75f15577fa0254daad383662d87501
Keerthi Sravan Ravi <ks3621@columbia.edu>
2021-03-12 07:18:27 -0500

    Update datagen
    - Removed B0, Gibbs (low-field), no artifact (low-field), off-res, rigid motion (rotation), low SNR

--- artifactID/datagen/gibbs_datagen.py
+++ artifactID/datagen/gibbs_datagen.py
@@ -12,13 +12,11 @@ def main(path_read_data: Path, path_save_data: Path, slice_size: int):
     # =========
     # PATHS
     # =========
-    if 'miccai' in str(path_read_data).lower():
-        arr_path_read = data_ops.glob_brats_t1(path_brats=path_read_data)
-    else:
-        arr_path_read = data_ops.glob_nifti(path=path_read_data)
+    arr_path_read_data = list(path_read_data.glob('noartifact\*'))
+
     path_save_data = Path(path_save_data)
     subjects_per_class = math.ceil(
-        len(arr_path_read) / len(arr_gibbs_range))  # Calculate number of subjects per class
+        len(arr_path_read_data) / len(arr_gibbs_range))  # Calculate number of subjects per class
 
     arr_gibbs_range = np.tile(arr_gibbs_range, subjects_per_class)
     np.random.shuffle(arr_gibbs_range)
@@ -26,37 +24,31 @@ def main(path_read_data: Path, path_save_data: Path, slice_size: int):
     # =========
     # DATAGEN
     # =========
-    for ind, path_t1 in tqdm(enumerate(arr_path_read[:75])):
-        vol = data_ops.load_nifti_vol(path_t1)
-        vol_resized = data_ops.resize(vol, size=slice_size)
-        vol_resized, idx = data_ops.__extract_brain(vol_resized,
-                                                    return_idx=True)  # Re-extract brain to obtain masking idx
-        vol_resized_normalized = data_ops.normalize_slices(vol=vol_resized)
-        chop = arr_gibbs_range[ind]
+    for ind, f in tqdm(enumerate(arr_path_read_data)):
+        sli = data_ops.load_preprocess_npy(f, slice_size)
+        sli_masked, mask_idx = data_ops.__extract_brain(sli, return_idx=True)  # Re-extract brain to obtain masking idx
+        sli_masked = np.expand_dims(sli_masked, axis=2)
+        fe_chop = arr_gibbs_range[ind]
 
-        kdat = np.fft.fftshift(np.fft.fftn(vol_resized_normalized))
+        # K-space
+        kdat = np.fft.fftshift(np.fft.fftn(sli_masked))
         num_pe_chop = np.random.randint(low=32, high=64)
-        rand_start = np.random.randint(low=0, high=160)
-        rand_stop = rand_start + num_pe_chop
-        # print(num_pe_chop, rand_start, rand_stop)
-        kdat[rand_start:rand_stop, :chop, :] = 0
-        kdat[rand_start:rand_stop, -chop:, :] = 0
-        vol_gibbs = np.abs(np.fft.ifftn(np.fft.ifftshift(kdat)))
-        vol_gibbs_masked = np.zeros_like(vol_gibbs)
-        vol_gibbs_masked[idx] = vol_gibbs[idx]
-        # Convert to float16 to avoid dividing by 0 during normalization - very low max values get zeroed out
-        # vol_gibbs = vol_gibbs_masked.astype(np.float16)
+        pe_chop_start = np.random.randint(low=0, high=160)
+        pe_chop_stop = pe_chop_start + num_pe_chop
+        kdat[pe_chop_start:pe_chop_stop, :fe_chop, :] = 0
+        kdat[pe_chop_start:pe_chop_stop, -fe_chop:, :] = 0
+        sli_gibbs = np.abs(np.fft.ifftn(np.fft.ifftshift(kdat)))
 
-        import sass
-        sass.scroll(vol_gibbs)
+        # Values outside brain should be 0
+        sli_gibbs_masked = np.zeros_like(sli_gibbs)
+        sli_gibbs_masked[mask_idx] = sli_gibbs[mask_idx]
+
+        # Convert to float16 to avoid dividing by 0 during normalization - very low max values get zeroed out
+        sli_gibbs = sli_gibbs_masked.astype(np.float16)
 
         # Save to disk
-        # _path_save = path_save_data.joinpath(f'gibbs{chop}')
-        # if not _path_save.exists():
-        #     _path_save.mkdir(parents=True)
-        # for i in range(vol_gibbs.shape[-1]):
-        #     _slice = vol_gibbs[..., i]
-        #     subject = path_t1.name.replace('.nii.gz', '')
-        #     _path_save2 = _path_save.joinpath(subject)
-        #     _path_save2 = str(_path_save2) + f'_slice{i}.npy'
-        #     np.save(arr=_slice, file=_path_save2)
+        _path_save = path_save_data.joinpath(f'gibbs{fe_chop}')
+        if not _path_save.exists():
+            _path_save.mkdir(parents=True)
+        _path_save = _path_save / f.name
+        np.save(arr=sli_gibbs, file=_path_save)
--- artifactID/datagen/main.py
+++ artifactID/datagen/main.py
@@ -2,13 +2,9 @@ import configparser
 import sys
 from pathlib import Path
 
-from sklearn.model_selection import train_test_split
-
 path_search = str(Path(__file__).parent.parent)  # To allow ORC to be discoverable
 sys.path.insert(0, path_search)
-from artifactID.datagen import fov_wrap_datagen, fov_wrap_datagen_z # offres_datagen, noartifact_datagen, rigidmotion_datagen, snr_datagen, \
-#     gibbs_datagen, nonrigidmotion_datagen, b0cartesian_datagen
-from artifactID.datagen import noartifact_datagen_godwin, gibbs_datagen_godwin, gibbs_datagen
+from artifactID.datagen import fov_wrap_datagen, fov_wrap_datagen_z, gibbs_datagen, noartifact_datagen
 
 # Read settings.ini configuration file
 path_settings = '../settings.ini'
@@ -24,41 +20,18 @@ test_split = float(config_data['test_split'])
 validation_split = float(config_data['validation_split'])
 
 # No-artifact datagen
-# print('No-artifact datagen...')
-# noartifact_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
-# noartifact_datagen_godwin.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
+print('No-artifact datagen...')
+noartifact_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
 
-# Ghosting datagen
+# FOV wrap-around datagen
 print('FOV wrap-around datagen (horizontal and vertical)...')
 fov_wrap_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
 print('FOV wrap-around datagen (slice direction)...')
 fov_wrap_datagen_z.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
 
-# Off-resonance datagen
-# print('\nOff-resonance datagen...')
-# offres_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, path_ktraj=path_ktraj,
-#                     path_dcf=path_dcf, patch_size=patch_size)
-
-# SNR datagen
-# print('\nSNR datagen...')
-# snr_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
-
-# Rotation datagen
-# print('\nRigid rotation datagen')
-# rigidmotion_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, patch_size=patch_size)
-
 # Gibbs datagen
 print('\nGibbs datagen...')
 gibbs_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
-# gibbs_datagen_godwin.main(arr_path_read=path_read_data, path_save_data=path_save_data, slice_size=slice_size)
-
-# Non-rigid motion datagen
-# print('\nNon-rigid motion datagen...')
-# nonrigidmotion_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, patch_size=patch_size)
-
-# B0 inhomogeneity Cartesian
-# print('\nB0 inhomogeneity Cartesian datagen...')
-# b0cartesian_datagen.main(path_read_data=path_read_data, path_save_data=path_save_data, patch_size=patch_size)
 
 # Create splits
 # _path_noartifact_folder = path_save_data / 'noartifact'
