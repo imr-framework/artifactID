5d4574df820495b6de892f88ce5d0a7206285b59
Keerthi Sravan Ravi <ks3621@columbia.edu>
2021-03-27 20:19:18 -0400

    Optimize imports, remove unused functions, modify generators
    - Optimize imports
    - Removed unused functions
    - Train-generator is now also eval-generator

--- artifactID/common/data_ops.py
+++ artifactID/common/data_ops.py
@@ -4,9 +4,6 @@ from pathlib import Path
 import cv2
 import nibabel as nb
 import numpy as np
-import pydicom as pyd
-from skimage.util import view_as_windows
-from tqdm import tqdm
 
 
 def __extract_brain(vol: np.ndarray, return_idx: bool = False):
@@ -35,68 +32,6 @@ def __extract_brain(vol: np.ndarray, return_idx: bool = False):
     return vol_masked
 
 
-def dcmfolder2npy(path: Path, verbose: bool = True):
-    arr_dcm = list(path.glob('*'))  # List all DICOM files
-    arr_npy = []
-    for dicom in tqdm(arr_dcm, disable=not verbose):
-        dcm = pyd.dcmread(str(dicom))
-        npy = dcm.pixel_array
-        arr_npy.append(npy)
-
-    try:
-        arr_npy = np.stack(arr_npy)
-        arr_npy = np.moveaxis(arr_npy, [0, 1, 2], [2, 0, 1])  # Scroll through slices along last dimension
-        return arr_npy
-    except:
-        pass
-
-
-def get_patches_per_slice(vol: np.ndarray, patch_size: int):
-    patches = view_as_windows(arr_in=vol, window_shape=[patch_size, patch_size, 1], step=[patch_size, patch_size, 1])
-    pruned_patches = []
-    patch_map = np.zeros(patches.shape[:3], dtype=np.int8)
-    np_index = np.ndindex(patches.shape[:3])
-    for multi_index in np_index:
-        p = patches[multi_index]
-        p = np.squeeze(p)
-        if np.count_nonzero(p) >= 0.5 * p.size and p.max() != p.min():  # Valid patch
-            pruned_patches.append(p)
-            patch_map[multi_index] = 1
-    return np.array(pruned_patches, dtype=np.float16), patch_map
-
-
-def get_y_label(data_root: Path) -> str:
-    """
-
-    Parameters
-    ==========
-    data_root : Path
-
-
-    Returns
-    =======
-    np.ndarray
-
-    """
-    y_labels = []
-    for artifact_folder in data_root.glob('*'):
-        if artifact_folder.is_dir():
-            label = artifact_folder.name
-            label = label.rstrip('0123456789').rstrip('-_')
-            y_labels.append(label)
-
-    y_labels.remove('noartifact')  # Remove no artifact label
-    y_labels = np.unique(y_labels)
-    if len(y_labels) > 2:
-        raise Exception('Too many artifact classes, expected only no-artifact and one other artifact class.')
-    return y_labels[0]
-
-
-def glob_dicom(path: Path):
-    arr_path = list(path.glob('**\*.dcm'))
-    return arr_path
-
-
 def glob_nifti(path: Path):
     arr_path = list(path.glob('**/*.nii.gz'))
     arr_path2 = list(path.glob('**/*.nii'))
@@ -109,7 +44,31 @@ def glob_brats_t1(path_brats: Path):
     return arr_path_brats_t1
 
 
-def load_nifti_vol(path: Path):
+def load_preprocess_npy(path: Path, target_size: int):
+    """
+    Read Numpy file at `path` and return an 3D numpy.ndarray.
+
+    Parameters
+    ==========
+    path : Path
+        Path to npy file to be read.
+
+    Returns
+    =======
+    numpy.ndarray
+        Numpy data of NIFTI file at `path`.
+    """
+    npy = np.load(str(path))
+    if len(npy.shape) == 3:
+        npy = resize_vol(npy, target_size)
+    else:
+        npy = resize(npy, target_size)
+    npy = __extract_brain(npy)
+    npy_normalized = (npy - npy.min()) / (npy.max() - npy.min())  # Normalize between 0-1
+    return npy_normalized.astype(np.float16)
+
+
+def load_nifti_vol(path: Path, target_size: int):
     """
     Read NIFTI file at `path` and return an 3D numpy.ndarray. Ensure correct orientation of the brain.
 
@@ -123,13 +82,14 @@ def load_nifti_vol(path: Path):
     numpy.ndarray
         Numpy data of NIFTI file at `path`.
     """
-    vol = nb.load(str(path)).get_fdata().astype(np.float16)
+    vol = nb.load(str(path)).get_fdata()
+    vol = resize_vol(vol, target_size)
     vol = __extract_brain(vol)
     vol = (vol - vol.min()) / (vol.max() - vol.min())  # Normalize between 0-1
-    return vol
+    return vol.astype(np.float16)
 
 
-def generator_train(x, dict_label_int):
+def generator_train_eval(x, mode: bytes = b'train'):
     """
     Training generator yielding volumes loaded from paths to .npy files in `x`. Also yields paired labels `y`. Labels
     are derived from the paths to the .npy files in `x`.
@@ -138,106 +98,52 @@ def generator_train(x, dict_label_int):
     ==========
     x : array-like
         Array of paths to .npy files to load.
-    dict_label_int : str
-        Dictionary mapping labels to integers. Used to derive `y` labels for each `x` input to model.
+    mode : bytes
+        Bytes array indicating if generator is used for training or evaluation. Datatype is bytes because Tensorflow
+        passes arguments as bytes in tf.data.Dataset
 
     Yields
     ======
-    dict{'input_1': np.ndarray, 'input_2': np.ndarray}
-        Dict of two volumes of shape (..., 1) and datatype np.float16.
+    _x : np.ndarray
+        K-space array of dimensions (x, y, 1) and datatype np.float16.
     _y : np.ndarray
-        Array containing a single corresponding label of datatype np.int8.
+        Array containing a single integer label indicating artifact-type of datatype np.int8.
     """
     counter = 0
+    mode = mode.decode()
     while True:
         path = x[counter]
+        if isinstance(path, bytes):
+            path = path.decode()
+        path = Path(path.strip())
         try:
-            path = Path(path.decode().strip())
             _x = np.load(str(path))  # Load volume
             _x = np.abs(np.fft.fftshift(np.fft.fftn(_x)))
             _x = np.squeeze(_x)
             _x = np.expand_dims(_x, axis=2)  # Convert shape to (..., 1)
             _x = _x.astype(np.float16)  # Mixed precision
 
-            # y integer label
-            if 'gibbs' in path.parent.name.lower():
-                _y = np.array([1], dtype=np.int8)
-            elif 'noartifact' in path.parent.name.lower():
-                _y = np.array([0], dtype=np.int8)
-            yield _x, _y
-        except Exception as e:
-            print(e)
-
-        counter += 1
-        if counter == len(x):  # Reset counter if end of array is reached
-            counter = 0
-
-
-def generator_evaluate(x: list, file_format: str):
-    """
-    Inference generator yielding volumes loaded from .npy files specified in `x`.
-
-    Parameters
-    ==========
-    x : array-like
-        Array of paths to .npy files to load.
-    file_format : str
-        File format of
-
-    Yields
-    ======
-    _x : np.ndarray
-        Array containing a single volume of shape (..., 1) and datatype np.float16.
-    """
-    for path_load in x:
-        try:
-            if file_format == 'nifti':
-                _x = nb.load(str(path_load)).get_fdata()
-            elif file_format == 'dicom':
-                _x = dcmfolder2npy(path=path_load)
+            if mode == 'eval' or mode == 'evaluate':
+                yield _x
             else:
-                raise Exception('Unhandled exception')
-            _x = __extract_brain(vol=_x)
-            _x = _x.astype(np.float16)  # Mixed precision
-            yield _x, path_load.parent.parts[-1]
-        except ValueError:
-            print(path_load)
+                # y = 0 for no artifact; y = 1 for Gibbs
+                folder = path.parent.name.lower()
+                if 'gibbs' in folder:
+                    _y = np.array([1], dtype=np.int8)
+                elif 'noartifact' in folder:
+                    _y = np.array([0], dtype=np.int8)
+                else:
+                    raise ValueError('Not training on Gibbs')
 
-
-def generator_inference(x: list):
-    """
-    Inference generator yielding volumes loaded from .npy files specified in `x`.
-
-    Parameters
-    ==========
-    x : array-like
-        Array of paths to .npy files to load.
-    file_format : str
-        File format of
-
-    Yields
-    ======
-    _x : np.ndarray
-        Array containing a single volume of shape (..., 1) and datatype np.float16.
-    """
-    counter = 0
-    while True:
-        path = x[counter]
-        try:
-            path = Path(path.decode().strip())
-            _x = np.load(str(path))  # Load volume
-            _x = np.abs(np.fft.fftshift(np.fft.fftn(_x)))
-            _x = np.squeeze(_x)
-            _x = np.expand_dims(_x, axis=2)  # Convert shape to (..., 1)
-            _x = _x.astype(np.float16)  # Mixed precision
-
-            yield _x
+                yield _x, _y
         except Exception as e:
             print(e)
 
         counter += 1
-        if counter == len(x):  # Reset counter if end of array is reached
-            break
+        if counter == len(x):
+            if mode == 'eval' or mode == 'evaluate':
+                break  # Break if end of array is reached during evaluation
+            counter = 0  # Reset counter if end of array is reached during training
 
 
 def normalize_slices(vol: np.ndarray):
@@ -256,41 +162,14 @@ def normalize_slices(vol: np.ndarray):
     _max = np.max(vol, axis=(0, 1))
     _min = np.min(vol, axis=(0, 1))
     _range = (_max - _min)
-    valid_idx = np.where(_max != _min)[0]
-    vol_normalized = (vol[..., valid_idx] - _min[valid_idx]) / _range[valid_idx]
+    valid_slices = np.where(_max != _min)[0]
+    vol_normalized = (vol[..., valid_slices] - _min[valid_slices]) / _range[valid_slices]
     return vol_normalized
-
-
-def patch_size_compatible_zeropad(vol: np.ndarray, patch_size: int):
-    """
-    Zero-pad input volume such that it is compatible for windowing of specified size.
-
-    Parameters
-    ==========
-    vol : np.ndarray
-        Input volume.
-    patch_size : int
-        Patch-size that `vol` needs ot be compatible with for windowing.
-
-    Returns
-    ======
-    np.ndarray
-        Zero-padded volume.
-    """
-    pad = []
-    shape = vol.shape
-    for s in shape[:2]:  # Pad per slice, not across volume
-        if s < patch_size or s % patch_size != 0:
-            p = patch_size - (s % patch_size)
-            pad.append((math.floor(p / 2), math.ceil(p / 2)))
-        else:
-            pad.append((0, 0))
-    pad.append((0, 0))  # Do not pad along last dimension
-    return np.pad(array=vol, pad_width=pad)
 
 
 def resize(sli: np.ndarray, size: int):
     return cv2.resize(sli.astype(np.float), (size, size))
+
 
 def resize_vol(vol: np.ndarray, size: int):
     vol_resized = []
@@ -301,17 +180,20 @@ def resize_vol(vol: np.ndarray, size: int):
     vol_resized = np.moveaxis(vol_resized, (0, 1, 2), (2, 0, 1))
     return vol_resized
 
+
 def vol_slice_pad(vol: np.ndarray, dim_2_pad: int):
     if dim_2_pad == 0:
         target_N = vol.shape[1]
     else:
         target_N = vol.shape[0]
     pad = target_N - vol.shape[dim_2_pad]
-    pad1, pad2 = math.ceil(pad/2), int(pad/2)
+    pad1, pad2 = math.ceil(pad / 2), int(pad / 2)
 
     if dim_2_pad == 0:
-        vol_padded = np.vstack((np.zeros((pad1, vol.shape[1], vol.shape[2])), vol, np.zeros((pad2, vol.shape[1], vol.shape[2]))))
+        vol_padded = np.vstack(
+            (np.zeros((pad1, vol.shape[1], vol.shape[2])), vol, np.zeros((pad2, vol.shape[1], vol.shape[2]))))
     else:
-        vol_padded = np.hstack((np.zeros((vol.shape[0], pad1, vol.shape[2])), vol, np.zeros((vol.shape[0], pad2, vol.shape[2]))))
+        vol_padded = np.hstack(
+            (np.zeros((vol.shape[0], pad1, vol.shape[2])), vol, np.zeros((vol.shape[0], pad2, vol.shape[2]))))
 
     return vol_padded
